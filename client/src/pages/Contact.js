15ca5c4b51a11649cab7afb29acdebbf0517d5c0
brinkster001 <ionut.iancu@mail.com>
2020-03-01 22:47:43 +0200

    some routes and some styles

---
+++ client/src/pages/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../css/Contact.scss';
+
+class Contact extends React.Component {
+    render() {
+      return (
+        <div id='contact'>
+            <div className='page-header'>
+                <h1 className='page-title'>Contact</h1>
+                <p className='page-description'>Page description</p>
+            </div>          
+        </div>
+      )
+    }
+  }
+
+export default Contact;
---
+++ client/src/pages/Services.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../css/Services.scss';
+
+class Services extends React.Component {
+    render() {
+      return (
+        <div id='services'>
+          <div className='page-header'>
+                <h1 className='page-title'>Services</h1>
+                <p className='page-description'>Page description</p>
+            </div>  
+        </div>
+      )
+    }
+  }
+
+export default Services;
